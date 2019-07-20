1324a9388dc45385009db9d4d3dd12d349e286fb
Abrar Al-Shaer <abrar@email.unc.edu>
2019-07-19 20:26:22 -0400

    Update seahorse_outlier_analysis.R

--- seahorse_outlier_analysis.R
+++ seahorse_outlier_analysis.R
@@ -1,109 +1,106 @@
+#MAKE SURE TO PROVIDE THE FINAL TRANSPOSED DATA FILE - same input for python seahorse program (seahorse_analysis.py)
 setwd("C:/Users/Abrar/Dropbox/UNC_OneDrive/Shaikh Lab/Seahorse/")
 rm(list=ls())
 
-#MAKE SURE TO PROVIDE THE FINAL TRANSPOSED DATA FILE - same input for python seahorse program (seahorse_analysis.py)
-ocr <- read.csv("SeahorseOCR_Data_COMBO_DAY21.csv", header = TRUE) #load in OCR data 
-ecar <- read.csv("SeahorseECAR_Data_COMBO_DAY21.csv", header = TRUE) #load in ECAR data
+#RUN THIS PROGRAM USING EITHER OCR OR ECAR FILES - AN EXAMPLE INPUT FILE "SeahorseECAR_Data_COMBO_DAY24_lean_HF_only" IS PLACED IN THE seahorse_analysis GITHUB FOLDER
+#Combo file of all biological replicates (all experiments for one timepoint)
+#ocr <- read.csv("Seahorse_Day10_ComboData_Waves1-3.csv", header = TRUE) #load in OCR data 
 
-#####OCR ANALYSIS
+#SeahorseOCR_Data_COMBO_DAY21_lean_HF_only
+#SeahorseOCR_Data_COMBO_DAY10_lean_HF_only & SeahorseECAR_Data_COMBO_DAY10_lean_HF_only
+ocr <- read.csv("SeahorseECAR_Data_COMBO_DAY24_lean_HF_only.csv", header = TRUE) #load in OCR data 
+file_name <- "SeahorseECAR_Data_COMBO_DAY24_lean_HF_only"
 
-groups = levels(ocr$Time..minutes.) #create a variable called groups for all levels (row groups) of the dataframe
-columns = colnames(ocr)[2:length(colnames(ocr))] #store columns in variable
+#####OUTLIER ANALYSIS
+
+groups = levels(ocr$Group) #create a variable called groups for all levels (row groups) of the dataframe
+columns = colnames(ocr)[3:length(colnames(ocr))] #store columns in variable
 
 datalist_ocr = list()
+outliers_data1 = list()
 c = 1
 
-#loop through each group (rows) for OCR data
+#loop through each group (rows) for COMBO data
 for(i in 1:length(groups)) {
   print(groups[i]) #print group
-  sub_ocr <- subset(ocr, ocr$Time..minutes. %in% groups[i]) #subset dataframe by the biological group
+  sub_ocr <- subset(ocr, ocr$Group %in% groups[i]) #subset dataframe by the biological group
   #loop through each column within each group
   for(j in 1:length(columns)){
-    #print(paste("COLUMN (Time Point):",columns[j]))
-    outliers_ocr <- boxplot.stats(sub_ocr[,columns[j]])$out #output of outlier values to a list, calculate outlier values based on the IQR formula (Q1-1.5*IQR, Q3+1.5*IQR)
-    #if there are outliers (list isn't empty)
-    if(length(outliers_ocr)>0)
-      {
-        datalist_ocr[[c]] <- c(groups[i],columns[j],boxplot.stats(sub_ocr[,columns[j]])$out)
-        c = c+1
+    print(paste("COLUMN:",columns[j]))
+    out1 = quantile(sub_ocr[,columns[j]], c(1)/4, na.rm = TRUE)[[1]]-1.5*IQR(sub_ocr[,columns[j]], na.rm = TRUE)
+    out1 = floor(out1*10)/10 #needed due to rounding error (this statement prevents rounding of the outlier cutoff so we don't detect very small deviations from the IQR)
+    out2 = quantile(sub_ocr[,columns[j]], c(3)/4, na.rm = TRUE)[[1]]+1.5*IQR(sub_ocr[,columns[j]], na.rm = TRUE)
+    #out2 = floor(out2*10)/10 #not necessary because it reduces the threshold for a high outlier (makes it so that we are too sensitive and detect very small deviations from the IQR)
+    n = 1
+    outliers_data1 = list()
+    for(k in sub_ocr[,columns[j]]){
+      if(is.nan(k) == FALSE && (k < out1 | k > out2)){
+        outliers_data1[[n]] = k
+        print(columns[j])
+        print(k)
+        n = n + 1
       }
+    }
+    #if there are outliers (list isn't empty)
+    if(length(outliers_data1)>0)
+    {
+      datalist_ocr[[c]] <- c(groups[i],columns[j],outliers_data1)
+      c = c+1
+      #outliers_data1 = list()
+    }
   }
 }
 
-#If the matrix is uneven the command below won't work - then just run matrix_data = as.matrix(datalist_ocr) below
-big_data_ocr = do.call(rbind, datalist_ocr) #combines all previous dataframes from for loop
-list_data = data.frame(datalist = unlist(datalist_ocr))
-matrix_data = as.matrix(datalist_ocr)
-#Check to see if the list is empty (no outliers) - if not, add column headers & send dataframe to a file
-if(length(big_data_ocr) < 1){
-  print("There are no OCR outliers")
-} else {
-  colnames(big_data_ocr) <- c("Sample", "Timepoint", "OCR Value")
-  write.csv(big_data_ocr, "seahorseOCR_outliers.csv")
-}
+#big_data_ocr = do.call(rbind, datalist_ocr) #combines all previous dataframes from for loop
+#list_data = data.frame(datalist = unlist(datalist_ocr))
 
-write.csv(matrix_data, "seahorseOCR_outliers_Day21_COMBO_Data.csv")
+#If big_data_data1 from rbind function fails
+matrix_data = as.matrix(datalist_ocr)
+write.csv(matrix_data, paste(file_name,"Outliers_7.10.19.csv"))
+
+#Check to see if the list is empty (no outliers) - if not, add column headers & send dataframe to a file
+# if(length(big_data_ocr) < 1){
+#   print("There are no OCR outliers")
+# } else {
+#   colnames(big_data_ocr) <- c("Sample", "Measurement", "Value")
+#   write.csv(big_data_ocr, "seahorseOCR_outliers.csv")
+# }
+
+#write.csv(matrix_data, "seahorseOCR_outliers_11.10.18.csv")
 
 #######replacing outliers with NaN
 
-#extracting all OCR values from the outliers list and placing them in a new list (removing first 2 elements for group & timepoint)
+#extracting all data1 values from the outliers list and placing them in a new list (removing first 2 elements for group & timepoint)
+#for the second list it's appending only element 2 - which is the column the outlier belongs to - useful for the next for loop when replacing outliers with NaNs
 datalist_ocr_new <- list()
+datalist_ocr_newNames <- list()
 count <- 1
 for (i in 1:length(datalist_ocr)){
   datalist_ocr_new[[count]] <- datalist_ocr[[i]][c(3:length(datalist_ocr[[i]]))]
+  datalist_ocr_newNames[[count]] <- datalist_ocr[[i]][2] #list of column names that contained outliers
   count = count + 1
 }
 
 #converting the list of lists into a single one level list
 datalist_ocr_new <- unlist(datalist_ocr_new)
+datalist_ocr_newNames <- unlist(datalist_ocr_newNames)
 
 #replacing the outliers step
-for (row in 1:length(ocr)) {
+#only loop through the list of outlier columns
+for (row in datalist_ocr_newNames) { 
   #print(row)
   #print(ocr[[row]])
   for (i in 1:length(ocr[[row]])){
     #print(ocr[[row]][i])
     for (j in 1:length(datalist_ocr_new))
-      if(ocr[[row]][i] == datalist_ocr_new[j]) {
+      if(is.nan(ocr[[row]][i]) == FALSE && (ocr[[row]][i] == datalist_ocr_new[j])) {
         ocr[[row]][i] <- "NaN" }
   }
 }
 
 #new dataframe without outliers
-write.csv(ocr, "seahorseOCR_Day21_COMBO_Data_RMoutliers.csv")
+write.csv(ocr, paste(file_name,"_RMoutliers_7.10.19.csv"))
 
-#####ECAR ANALYSIS
-
-groups = levels(ecar$Time..minutes.) #create a variable called groups for all levels (row groups) of the dataframe
-columns = colnames(ecar)[2:length(colnames(ecar))] #store columns in variable
-
-datalist_ecar = list()
-c = 1
-
-#loop through each group (rows) for OCR data
-for(i in 1:length(groups)) {
-  print(groups[i]) #print group
-  sub_ecar <- subset(ecar, ecar$Time..minutes. %in% groups[i]) 
-  #loop through each column within each group
-  for(j in 1:length(columns)){
-    outliers_ecar <- boxplot.stats(sub_ocr[,columns[j]])$out
-    #if there are outliers (list isn't empty)
-    if(length(outliers_ecar)>0)
-    {
-      datalist_ecar[[c]] <- c(groups[i],columns[j],boxplot.stats(sub_ecar[,columns[j]])$out)
-      c = c+1
-    }
-  }
-}
-
-big_data_ecar = do.call(rbind, datalist_ecar) #combines all previous dataframes from for loop
-#Check to see if the list is empty (no outliers) - if not, add column headers & send dataframe to a file
-if(length(big_data_ecar) < 1){
-  print("There are no ECAR outliers")
-} else {
-  colnames(big_data_ecar) <- c("Sample", "Timepoint", "ECAR Value")
-  write.csv(big_data_ecar, "seahorseECAR_outliers.csv") 
-}
 
 #calculate outlier values based on the IQR formula MANUALLY
 #quantile(sub$X1.413664)[2]-1.5*IQR(sub$X1.413664) Q1-1.5*IQR
